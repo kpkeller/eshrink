65190443599958c295f232f50446bef2e933f245
jpkeller <jkelle46@jhu.edu>
2017-11-08 21:00:35 -0500

    added help file for package

---
+++ R/eshrink.R
@@ -0,0 +1,10 @@
+##' @name eshrink-package
+##' @title Shrinkage Estimators for Regression
+##' @docType package
+##' @aliases eshrink eshrink-package
+##' @description Computes shrinkage estimators for regression problems. Selects
+##' penalty parameter by minimizing bias and variance in the effect estimate,
+##' where bias and variance are estimated from the posterior predictive
+##'  distribution. See Keller and Rice (2017) <doi:10.1093/aje/kwx225> for more 
+##' details.
+NULL
